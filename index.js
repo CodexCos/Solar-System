ac5de0dad3528bae2bc85483f513958ae7246576
CodexCos <132887144+CodexCos@users.noreply.github.com>
2023-09-30 17:51:46 +0545

    Update index.js

--- index.js
+++ index.js
@@ -33,55 +33,55 @@ button.addEventListener('click', () =>{
         let result = inputValue * 8.87;
         error.textContent = `${result.toFixed(2)} N`;
         img.style.display = 'inline';
-        img.src = './images/Venus.png';
+        img.src = './images/venus.png';
     }
     else if(select.value === '3'){
         let result = inputValue * 9.807;
         error.textContent = `${result.toFixed(2)} N`;
         img.style.display = 'inline';
-        img.src = './images/Earth.png';
+        img.src = './images/earth.png';
     }
     else if(select.value === '4'){
         let result = inputValue * 1.62;
         error.textContent = `${result.toFixed(2)} N`;
         img.style.display = 'inline';
-        img.src = './images/Moon.png';
+        img.src = './images/moon.png';
     }
     else if(select.value === '5'){
         let result = inputValue * 3.71;
         error.textContent = `${result.toFixed(2)} N`;
         img.style.display = 'inline';
-        img.src = './images/Mars.png';
+        img.src = './images/mars.png';
     }
     else if(select.value === '6'){
         let result = inputValue * 24.79;
         error.textContent = `${result.toFixed(2)} N`;
         img.style.display = 'inline';
-        img.src = './images/Jupiter.png';
+        img.src = './images/jupiter.png';
     }
     else if(select.value === '7'){
         let result = inputValue * 10.44;
         error.textContent = `${result.toFixed(2)} N`;
         img.style.display = 'inline';
-        img.src = './images/Saturn.png';
+        img.src = './images/saturn.png';
     }
     else if(select.value === '8'){
         let result = inputValue * 8.87;
         error.textContent = `${result.toFixed(2)} N`;
         img.style.display = 'inline';
-        img.src = './images/Uranus.png';
+        img.src = './images/uranus.png';
     }
     else if(select.value === '9'){
         let result = inputValue * 11.15;
         error.textContent = `${result.toFixed(2)} N`;
         img.style.display = 'inline';
-        img.src = './images/Neptune.png';
+        img.src = './images/neptune.png';
     }
     else if(select.value === '10'){
         let result = inputValue * 0.62;
         error.textContent = `${result.toFixed(2)} N`;
         img.style.display = 'inline';
-        img.src = './images/Pluto.png';
+        img.src = './images/pluto.png';
     }
 
 })
